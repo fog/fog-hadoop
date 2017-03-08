4aaf9e3a822fdbb03de19ef4e97d3bb16df58d2d
Alberto Jesus Gutierrez Juanes <aljesusg@gmail.com>
2017-03-07 18:09:34 -0800

    Implemented get nodes information

---
+++ lib/fog/hadoop/requests/compute/get_node.rb
@@ -0,0 +1,38 @@
+module Fog
+  module Compute
+    class Hadoop
+      class Real
+        def get_node(node_ref)
+          request(
+              :expects => [200, 203],
+              :method => 'GET',
+              :path => "#{Fog::Hadoop.yarn_endpoint}/nodes/#{node_ref}"
+          ).body["node"]
+        end
+      end
+      class Mock
+        def get_node(node_ref)
+          response = Excon::Response.new
+          response.status = 200
+          response.body =
+              {
+                  "rack"                  => "\/default-rack",
+                  "state"                 => "NEW",
+                  "id"                    => "h2:1235",
+                  "nodeHostName"          => "h2",
+                  "nodeHTTPAddress"       => "h2:2",
+                  "healthStatus"          => "Healthy",
+                  "lastHealthUpdate"      => 1324056895432,
+                  "healthReport"          => "Healthy",
+                  "numContainers"         => 0,
+                  "usedMemoryMB"          => 0,
+                  "availMemoryMB"         => 5120,
+                  "usedVirtualCores"      => 0,
+                  "availableVirtualCores" => 8
+              }
+          response
+        end
+      end
+    end
+  end
+end
